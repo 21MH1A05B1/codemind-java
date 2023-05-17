67144196988bec697d89626fc8bdba17103bfd6d
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2023-05-17 21:08:28 +0530

    import java.util.*; public class java {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String n;         n=sc.nextLine();         System.out.println("Hello Technicalhub");
            System.out.println(n);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class java
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n;
+        n=sc.nextLine();
+        System.out.println("Hello Technicalhub");
+
+        System.out.println(n);
+    }
+}
